0c4d3cb64db33555ef990e57ec1819a2c1ad9628
Fredy Roman Vazquez Geronimo <frvazquez@MacBook-Pro-de-Fredy.local>
2024-05-02 19:18:49 -0600

    1.0.2 se actualiza proyecto con jpa y consulta de api de libtros, y autores

--- src/main/java/com/frvazquez/app/ChallengeLiteraturaApplication.java
+++ src/main/java/com/frvazquez/app/ChallengeLiteraturaApplication.java
@@ -1,11 +1,21 @@
 package com.frvazquez.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.frvazquez.app.client.ClienteLiteratura;
+import com.frvazquez.app.repository.AutorRepository;
+import com.frvazquez.app.repository.LibroRepository;
+
 @SpringBootApplication
 public class ChallengeLiteraturaApplication implements CommandLineRunner {
+
+	@Autowired
+	private LibroRepository libroRepositorio;
+	@Autowired
+	private AutorRepository autorRepositorio;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeLiteraturaApplication.class, args);
@@ -13,8 +23,9 @@ public class ChallengeLiteraturaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
+
+		ClienteLiteratura client = new ClienteLiteratura(libroRepositorio, autorRepositorio);
+		client.meunu();
 	}
 
 }
